6d32df9f46db245dc7324e58a3b2e7cc2d7df90d
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-01-13 16:16:30 -0500

    answered main.js questions

---
+++ main.js
@@ -0,0 +1,55 @@
+var name;
+// Q: What does name equal right now?
+
+// A: Name equals undefined right now. The variable has been created, however it hasn't been assigned to anything.
+
+
+name = "Tim";
+// Q: What is `name` set to right now?
+
+// A: Name is set to equal the string "Tim" right now.
+
+
+name = "Bill";
+// Q: What is `name` set to right now?
+
+// A: Name is set to equal the string "Bill" right now.
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+
+// A: Greeting is set to equal the string "Hello, how are you?"
+
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+
+// A: math is set to the function (high - low), which at this time will return 40 the number 40.
+
+
+math = high - "5";
+// Q: What is `math` set to?
+
+// A: math is set to the function (high - "5"), which at this time will return an error because a number value can't be subtracted by a string value.
+
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1984;
+var today = 2015;
+
+var my_age = "Tim is " + (today - born) + " years old"
+
+
+// Store some information following in variables.
+var yourname; "Spencer"
+var myname; "Tim"
+
+var statement = yourname + " is taking a class at The Iron Yard, and my
+instructor is " + myname;
+
